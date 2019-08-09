dad4f81737905cdc7e043b7e952bcc25a1d7b02c
Edward Dumholt <edumholt@gmail.com>
2019-08-08 19:21:05 -0500

    clean up messages, app.component

--- src/starter files/messages.ts
+++ src/starter files/messages.ts
@@ -1,34 +1,33 @@
 import { Message } from '../../src/app/message';
 
-export const MESSAGES: Message[] = 
-
-[
-    {
-        id: 1,
-        from: 'Douglas',
-        subject: 'JavaScript: The Good Parts',
-        unread: true,
-        selected: false
-    },
-    {
-        id: 2,
-        from: 'Deborah',
-        subject: 'Angular Reactive Forms',
-        unread: false,
-        selected: false
-    },
-    {
-        id: 3,
-        from: 'Dan',
-        subject: 'React with Redux',
-        unread: false,
-        selected: false
-    },
-    {
-        id: 4,
-        from: 'John',
-        subject: 'Advanced TypeScript',
-        unread: false,
-        selected: false
-    }
-]
+export const MESSAGES: Message[] =
+    [
+        {
+            id: 1,
+            from: 'Douglas',
+            subject: 'JavaScript: The Good Parts',
+            unread: true,
+            selected: false
+        },
+        {
+            id: 2,
+            from: 'Deborah',
+            subject: 'Angular Reactive Forms',
+            unread: false,
+            selected: false
+        },
+        {
+            id: 3,
+            from: 'Dan',
+            subject: 'React with Redux',
+            unread: false,
+            selected: false
+        },
+        {
+            id: 4,
+            from: 'John',
+            subject: 'Advanced TypeScript',
+            unread: false,
+            selected: false
+        }
+    ];
